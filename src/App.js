19977e45c7c21e0ad64a8fbadcf093953e6d16d2
Kane Toomer <kanetoomer@yahoo.com>
2022-06-18 14:30:22 -0700

    particles added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import Particles from './components/Particles/Particles';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles />
         <Navigation />
         <Logo />
+        <Rank />
         <ImageLinkForm />
         {/* <FaceRecognition /> */}
       </div>
