b9a62a519e4dc70f6ca985fb10f94c1cf03cce2a
Kane Toomer <kanetoomer@yahoo.com>
2022-06-19 12:07:47 -0700

    added in Clarifai API, starting face detection

--- src/App.js
+++ src/App.js
@@ -6,9 +6,47 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+
+import Clarifai, { FACE_DETECT_MODEL } from 'clarifai';
+
+
+const app = new Clarifai.App({
+  apiKey: 'c846681a4ece4eb4b4d24347feb9d69b'
+});
+
 
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value })
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input })
+    app.models
+      .predict(
+        FACE_DETECT_MODEL,
+        this.state.input)
+      .then(
+        function (response) {
+          console.log(response);
+        },
+        function (err) {
+
+        }
+      );
+  }
+
   render() {
     return (
       <div className="App">
@@ -16,8 +54,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
-        {/* <FaceRecognition /> */}
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl} />
       </div>
     );
   }
--- src/components/Particles/Particles.js
+++ src/components/Particles/Particles.js
@@ -26,28 +26,28 @@ const ParticlesBg = () => {
             loaded={particlesLoaded}
             options={{
                 fpsLimit: 120,
-                interactivity: {
-                    events: {
-                        onClick: {
-                            enable: true,
-                            mode: "push",
-                        },
-                        onHover: {
-                            enable: true,
-                            mode: "repulse",
-                        },
-                        resize: true,
-                    },
-                    modes: {
-                        push: {
-                            quantity: 4,
-                        },
-                        repulse: {
-                            distance: 200,
-                            duration: 0.4,
-                        },
-                    },
-                },
+                // interactivity: {
+                //     events: {
+                //         onClick: {
+                //             enable: true,
+                //             mode: "push",
+                //         },
+                //         onHover: {
+                //             enable: true,
+                //             mode: "repulse",
+                //         },
+                //         resize: true,
+                //     },
+                //     modes: {
+                //         push: {
+                //             quantity: 4,
+                //         },
+                //         repulse: {
+                //             distance: 200,
+                //             duration: 0.4,
+                //         },
+                //     },
+                // },
                 particles: {
                     color: {
                         value: "#ffffff",
